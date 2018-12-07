4de5872de381bf7074f6c71e7903e3f6f432ee06
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-07 16:21:53 +0100

    Параметризация метода login

--- test_add_group.py
+++ test_add_group.py
@@ -15,7 +15,7 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password= "secret")
         self.open_groups_page(wd)
         self.create_group(wd)
         self.return_to_groups_page(wd)
@@ -46,12 +46,12 @@ class TestAddGroup(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):

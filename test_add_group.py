9de375db0726661ed59faa23a0da655c2ef4da90
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-07 16:27:20 +0100

    Параметризация метода create_group

--- test_add_group.py
+++ test_add_group.py
@@ -15,9 +15,9 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd, username="admin", password= "secret")
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="ttt", header="ggg", footer="bbb")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -27,19 +27,19 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init_group_creation
         wd.find_element_by_name("new").click()
         # fill_group_form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("ttt")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("ggg")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("bbb")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit_group_creation
         wd.find_element_by_name("submit").click()
 

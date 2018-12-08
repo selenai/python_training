89f4a8ea3029d056ea9d766291de03c03b6c6fcc
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-08 22:40:45 +0100

    Выделен вспомогательный класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="ttt", header="ggg", footer="bbb"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()

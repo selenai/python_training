1802a36d58b73dadfa8415c4bafb0250ce72436c
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-08 16:02:46 +0100

    Создание функции инициализирующей фикстуру, удаление тестового класса, полный переход на запуск при помощи pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,25 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="ttt", header="ggg", footer="bbb"))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="ttt", header="ggg", footer="bbb"))
+    app.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()

ea04cf12b05faaa6f0a6d1a97917885de9fa2280
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-10 22:30:48 +0100

    Добавлен тест: Удаление контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -80,3 +80,13 @@ class ContactHelper:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        # confirm deletion
+        wd.switch_to_alert().accept()
+

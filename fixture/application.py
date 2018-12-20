5b353c8fb8a1023a053bc724cddcabe3f4e08d1e
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-20 12:38:41 +0100

    Добавлена проверка текущего состояния сессии

--- fixture/application.py
+++ fixture/application.py
@@ -8,14 +8,14 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
     def is_valid(self):
         try:
-            self.wd.curent_url
+            self.wd.current_url
             return True
         except:
             return False

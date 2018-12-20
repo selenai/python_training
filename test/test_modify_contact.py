f3dcb910766bb8cda70275bdea439988d864fc6a
Alena Silchanka <alenasilchanka@gmail.com>
2018-12-20 11:05:34 +0100

    Добавлена проверка валидности фикстуры

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,12 +3,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="New first name"))
-    app.session.logout()
 
 
 def test_modify_contact_nickname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(nickname="New nickname"))
-    app.session.logout()

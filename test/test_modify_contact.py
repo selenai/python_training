ca7d140946cd40df1ebcf2ee54b4dd260ac947e7
Alena Silchanka <alenasilchanka@gmail.com>
2019-01-10 10:40:25 +0100

    Добавлено поэлементное сравнение 2-х списков контактов (модификация контакта)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,16 +6,19 @@ def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(firstname="1TestContact"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="New first name"))
+    contact = Contact(firstname="New first name")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-
-def test_modify_contact_nickname(app):
-    if app.contact.count() == 0:
-        app.contact.create_contact(Contact(firstname="1TestContact"))
-    old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(nickname="New nickname"))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+#def test_modify_contact_nickname(app):
+#    if app.contact.count() == 0:
+#        app.contact.create_contact(Contact(firstname="1TestContact"))
+#    old_contacts = app.contact.get_contact_list()
+#    app.contact.modify_first_contact(Contact(nickname="New nickname"))
+#    new_contacts = app.contact.get_contact_list()
+#    assert len(old_contacts) == len(new_contacts)

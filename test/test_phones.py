ac7906fb46ada96b5c71bb3092cc75947c854937
Alena Silchanka <alenasilchanka@gmail.com>
2019-01-14 17:39:32 +0100

    Реализованы проверки для следующих полей случайно выбранного контакта на главной странице (фамилия, имя, адрес, емэйлы, телефоны)

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,12 @@
 import re
+from random import randrange
 
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contact_list = app.contact.get_contact_list()
+    index = randrange(len(contact_list))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.address == contact_from_edit_page.address

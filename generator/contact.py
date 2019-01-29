94ea59003af5a6aaa95624f3db88d1690282cd65
Alena Silchanka <alenasilchanka@gmail.com>
2019-01-29 22:31:33 +0100

    Реализована загрузка тестовых данных из файлов в формате json

--- generator/contact.py
+++ generator/contact.py
@@ -41,5 +41,5 @@ testdata = [Contact(firstname="", lastname="", address="", home="", mobile="", w
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-#    out.write(jsonpickle.encode(testdata))
-    out.write(json.dumps(json.loads(jsonpickle.encode(testdata)), default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
